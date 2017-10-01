59c6cf2792c16bde12279529985a088d440cb56e
Eloisa Santos <eloisasantos@live.com>
2017-09-30 21:13:36 -0300

    Initial commit

---
+++ app/src/androidTest/java/usjt/arqdesis/br/aula14/ActivityATest.java
@@ -0,0 +1,10 @@
+package usjt.arqdesis.br.aula14;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Eloisa on 30/09/2017.
+ */
+public class ActivityATest {
+
+}
---
+++ app/src/main/java/usjt/arqdesis/br/aula14/StatusTracker.java
@@ -0,0 +1,8 @@
+package usjt.arqdesis.br.aula14;
+
+/**
+ * Created by Eloisa on 30/09/2017.
+ */
+
+class StatusTracker {
+}
